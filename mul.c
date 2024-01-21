283693c5129b86d9819091a1c7ac191618a95c04
Nkuli0109 <n.sikali@gmail.com>
2024-01-21 13:23:50 -0800

    updated file

--- mul.c
+++ mul.c
@@ -1,5 +1,6 @@
+#include "monty.h"
 /**
- * f_mul - multiplies the top two elements of the stack
+ * f_mul - multiplies the top two elements of the stack.
  * @head: stack head
  * @counter: line_number
  * Return: no return
@@ -22,7 +23,7 @@ void f_mul(stack_t **head, unsigned int counter)
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
-		exit(EXIT_FAILURE);
+		exit(EXIT_FAILFURE);
 	}
 	h = *head;
 	aux = h->next->n * h->n;
