6c9618ac4316c57831bae30372dfa5297d62dc99
Nkuli0109 <n.sikali@gmail.com>
2024-01-21 13:06:56 -0800

    updated files

--- mul.c
+++ mul.c
@@ -1,9 +1,9 @@
 /**
  * f_mul - multiplies the top two elements of the stack
  * @head: stack head
- * @counter: line number
+ * @counter: line_number
  * Return: no return
- * */
+ */
 
 void f_mul(stack_t **head, unsigned int counter)
 {
